8c22586430ee37356d5268d793a71a4c9ec9bd7d
DavideTessari <davtex@gmail.com>
2024-06-11 20:15:10 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import PostsList from './components/PostsList.vue';
+
+export default {
+  components: {
+    PostsList
+  }
+}
+</script>
+
+<template>
+  <PostsList></PostsList>
+</template>
+
+<style lang="scss">
+@use './style/generic';
+</style>
